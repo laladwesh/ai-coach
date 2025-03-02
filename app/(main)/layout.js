a55e83f92f1a7ab4616971b3b4ebf58556444555
Avinash Gupta <guptaavinash302@gmail.com>
2025-03-02 10:35:04 +0530

    major layout ui changes added some pages

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 const MainLayout = async ({ children }) => {
-  return <div className="container mx-auto mt-24 mb-20">{children}</div>;
+  return <div className="container mx-auto mt-24 px-8 mb-20">{children}</div>;
 };
 
 export default MainLayout;
