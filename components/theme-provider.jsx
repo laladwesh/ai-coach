965e6e444ab8ab8d5dfad5dfb5dd02bad4906a17
laladwesh <guptaavinash302@gmail.com>
2025-06-13 14:50:31 +0530

    theme configured

--- components/theme-provider.jsx
+++ components/theme-provider.jsx
@@ -1,8 +1,14 @@
-"use client";
+"use client"; // Ensures this component runs only on the client side
 
 import * as React from "react";
 import { ThemeProvider as NextThemesProvider } from "next-themes";
 
+// A wrapper around next-themes' ThemeProvider to enable theme switching (light/dark/system)
 export function ThemeProvider({ children, ...props }) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>;
+  return (
+    // Pass down all props (like defaultTheme, attribute, enableSystem, etc.)
+    <NextThemesProvider {...props}>
+      {children}
+    </NextThemesProvider>
+  );
 }
